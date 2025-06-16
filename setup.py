40b71e87db416742fb3314a93cf52e3a8e008a1f
whybirdslie <whybirdslie@gmail.com>
2025-06-16 10:26:46 +0200

    MIT licenses added to files

--- setup.py
+++ setup.py
@@ -16,5 +16,18 @@ setup(
         'console_scripts': [
             'findyourtab=findyourtab_native:main',
         ],
-    }
+    },
+    author="whybirdslie",
+    author_email="whybirdslie@gmail.com",
+    description="Universal Browser Tab Manager for Windows",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/whybirdslie/findyourtab",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires=">=3.6",
+    license="MIT",
 ) 
